4f83b644adfc4de9e6eccd17a6308428d5a4d193
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-04-25 14:29:14 +0500

    push to github

---
+++ Q34.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// creating an array
+let pizza = ["chicken fajita", "super supreme", "peproni"];
+// Using for loop 
+for (let onePizza of pizza) {
+    console.log(`I would love to eat ${onePizza}`);
+}
+// print a message out side the for loop
+console.log("pizza is love");
---
+++ Q34.ts
@@ -0,0 +1,8 @@
+// creating an array
+let  pizza = ["chicken fajita" ,"super supreme","peproni"];
+// Using for loop 
+for(let onePizza of pizza){
+console.log(`I would love to eat ${onePizza}`);
+}
+// print a message out side the for loop
+console.log( "pizza is love")
